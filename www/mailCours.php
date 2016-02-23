227b24e851023725a986728af5510db094140a40
Antoine Leclair <antoineleclair@gmail.com>
2016-02-23 13:36:51 -0500

    Use env variable for ADD email address

--- www/mailCours.php
+++ www/mailCours.php
@@ -1,8 +1,11 @@
 <?php
- 
+
+require_once('../bootstrap.php');
+define('ADD_EMAIL', getenv('ADD_EMAIL'));
+ADD_EMAIL or die('Email mal configuré');
+
 if( /* Si j'ai bien reçu tout le formulaire sans champ manquant */ isset($_POST['nom']) AND isset($_POST['mail']) AND isset($_POST['tel']) AND isset($_POST['cours'])){
 
-	$destinataire = "res.addquebec@gmail.com";
 	$sujet = $_POST['cours']." ".$_POST['nom']. " ". $_POST['tel'] ;
     $message = $_POST['mail']. " ". $_POST['tel'] ;
     $headers = "MIME-Version: 1.0" . "\r\n";
@@ -13,11 +16,10 @@ if( /* Si j'ai bien reçu tout le formulaire sans champ manquant */ isset($_POST
 	$headers2 .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 	$headers2 .= "From: noreply@addquebec.ca"."\r\n";
 
-mail($destinataire, $sujet, $message, $headers);
+mail(ADD_EMAIL, $sujet, $message, $headers);
 
 header("Location: http://www.addquebec.ca/inscription.php");
 
 }
 
 ?>
-
